2e5fcdfc530e09c3be70309de6d82ec95285789b
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-11-25 14:25:36 +0530

    u

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.6.0"
-    }
-  }
-}
-
-provider "google" {
-  credentials = file("terraform_credentails.json")
-  project     = var.project_id
-  region      = "us-central1"
-  zone        = "us-central1-a"
-}
-
 resource "google_dns_managed_zone" "my_zone" {
   name          = "my-cloudrroot7-domain-zone"
   dns_name      = "cloudroot7.xyz."
